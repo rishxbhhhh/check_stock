570cf4e471357454c603a2f59a4c1fe7800a22d0
Rishabh Rajpurohit <rajpurohitrishabh1@gmail.com>
2025-07-13 00:01:42 +0530

    bug in ec2 server fixed

--- main.py
+++ main.py
@@ -124,9 +124,11 @@ def check_telegram_commands():
 
 # Setup browser
 options = Options()
-options.add_argument("--headless")
+options.add_argument("--headless=new")
 options.add_argument("--disable-gpu")
 options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--remote-debugging-port=9222")
 driver = webdriver.Chrome(options=options)
 
 try:
